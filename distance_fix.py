5faa3fe91b37dd78a5c02c55c01dbfdec7589ff8
sirdrops <s.misiewicz92@gmail.com>
2019-07-03 11:43:30 +0100

    upload distance_fix for python3

---
+++ distance_fix.py
@@ -0,0 +1,38 @@
+import serial
+import time
+
+ser = serial.Serial(
+	port='/dev/ttyS0',
+	baudrate=9600,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS
+)
+err = False
+
+def read_dist(bit1,bit2):
+	global err
+	try:
+		data = b'\x22\x00\x00\x22'
+		ser.write(data)
+		data = ser.readline(4)
+		print(data[0],data[1],data[2],data[3])
+		distvalue = data[bit1] << 8
+		distvalue = int(distvalue) + int(data[bit2])
+		print(distvalue)
+	except Exception as e:
+		print(e)
+		print("error, bit move one right all")
+		err = True
+
+
+while True:
+	time.sleep(0.2)
+
+	read_dist(1,2)
+
+	while err:
+
+		time.sleep(0.2)
+
+		read_dist(2,3)
